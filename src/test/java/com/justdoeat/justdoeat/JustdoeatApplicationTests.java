87200fd94a8c3d92bd3bfb5eb735e13c1e14d8d2
alexsyy <76884549+alexsyy@users.noreply.github.com>
2024-04-14 12:36:34 +0200

    Project init

---
+++ src/test/java/com/justdoeat/justdoeat/JustdoeatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.justdoeat.justdoeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustdoeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
