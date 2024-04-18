e4dc31503433becdc4b76b9cbd699eb4f8c71dd0
Djellali <katiadjellali05@gmail.com>
2024-04-18 11:24:24 +0200

    Update ListeCourse Controller

--- src/ListeCourse/Controller.java
+++ src/ListeCourse/Controller.java
@@ -1,27 +1,56 @@
 package ListeCourse;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
+@RestController
+@RequestMapping("/api/listes-courses")
 public class Controller {
-    private Service listeCourseService = new Service();
 
-    public List<ListeCourse> getAllListesCourses() {
-        return listeCourseService.getAllListesCourses();
+    private final Service listeCourseService;
+
+    public Controller(Service listeCourseService) {
+        this.listeCourseService = listeCourseService;
     }
 
-    public ListeCourse getListeCourseById(Long id) {
-        return listeCourseService.getListeCourseById(id);
+    @GetMapping
+    public ResponseEntity<List<ListeCourse>> getAllListesCourses() {
+        List<ListeCourse> listesCourses = listeCourseService.getAllListesCourses();
+        return new ResponseEntity<>(listesCourses, HttpStatus.OK);
     }
 
-    public ListeCourse createListeCourse(ListeCourse listeCourse) {
-        return listeCourseService.createListeCourse(listeCourse);
+    @GetMapping("/{id}")
+    public ResponseEntity<ListeCourse> getListeCourseById(@PathVariable Long id) {
+        ListeCourse listeCourse = listeCourseService.getListeCourseById(id);
+        if (listeCourse != null) {
+            return new ResponseEntity<>(listeCourse, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
-    public ListeCourse updateListeCourse(Long id, ListeCourse listeCourse) {
-        return listeCourseService.updateListeCourse(id, listeCourse);
+    @PostMapping
+    public ResponseEntity<ListeCourse> createListeCourse(@RequestBody ListeCourse listeCourse) {
+        ListeCourse createdListeCourse = listeCourseService.createListeCourse(listeCourse);
+        return new ResponseEntity<>(createdListeCourse, HttpStatus.CREATED);
     }
 
-    public void deleteListeCourse(Long id) {
+    @PutMapping("/{id}")
+    public ResponseEntity<ListeCourse> updateListeCourse(@PathVariable Long id, @RequestBody ListeCourse listeCourse) {
+        ListeCourse updatedListeCourse = listeCourseService.updateListeCourse(id, listeCourse);
+        if (updatedListeCourse != null) {
+            return new ResponseEntity<>(updatedListeCourse, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteListeCourse(@PathVariable Long id) {
         listeCourseService.deleteListeCourse(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
